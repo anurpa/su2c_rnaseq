9fa6392f2bf154de5bc4d5596a1e28e2709b7a1c
anurpa <anurpa@ohsu.edu>
2021-06-16 16:33:56 -0700

    Delete #!/bin/bash  # #$ -cwd #$ -j y #$ -S/bin/bash #$ -pe orte 10 #$ -V  cufflinks-1.3.0.Linux_x86_64/cufflinks -p 10 -o outputFileName -G path/to/UCSC_Genes.gtf  path/to/tophatOutput directory

--- #!/bin/bash  # #$ -cwd #$ -j y #$ -S/bin/bash #$ -pe orte 10 #$ -V  cufflinks-1.3.0.Linux_x86_64/cufflinks -p 10 -o outputFileName -G path/to/UCSC_Genes.gtf  path/to/tophatOutput/cufflinks.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash 
-#
-#$ -cwd
-#$ -j y
-#$ -S /bin/bash
-#$ -pe orte 10
-#$ -V
-
-cufflinks-1.3.0.Linux_x86_64/cufflinks -p 10 -o outputFileName -G path/to/UCSC_Genes.gtf  path/to/tophatOutput/accepted_hits.bam
